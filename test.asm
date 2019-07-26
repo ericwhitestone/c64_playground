be4cec014048a154204a0fddf7fb746ae1bad59e
Eric Whitestone <eric.whitestone@gmail.com>
2019-07-25 23:45:56 -0400

    Write a 64 byte sprite

--- test.asm
+++ test.asm
@@ -6,18 +6,39 @@
 BORDER = $d020
 SCREEN = $d021
 PRINTSR = $ffd2
-
-start   inc SCREEN  ; increase screen colour 
-        inc BORDER  ; increase border colour
-        jsr delay
-        jmp start   ; repeat
+SPRITE0_PTR = $07F8
 
 
-delay    ldy #$0
-rollover inx
-         bne rollover
-         iny
-         tya 
-         jsr PRINTSR
-         bne rollover
-         rts
+write_pattern   lda #$07        ;store the byte pattern for the sprite
+                ldy #$0
+write_byte      sta $4640, Y      ; $8(segment multplier)*$64 + $4000
+                iny             ; increment the idx
+                cpy #$64        ; check if 64 bytes have been written
+                bne write_byte  ; keep going until we hit 64 bytes
+                rts
+
+        ldx #$0     ;set x to 0
+        stx SCREEN ;start screen color at 0
+        stx BORDER ;start border color at 0
+        jsr write_pattern ;write the sprite pattern in the 64 sprite bytes
+        ldx #$25 ;store 8 in x,
+        stx SPRITE0_PTR ; write 8 to be the value of the sprite pointer,
+                        ; this is the value that is used to derive $200 used as the start of the sprite data
+        ldx $FFFF
+loop    stx $D015   ; write FFFF to sprite enable 
+        jmp loop
+
+;start_cycle     inc SCREEN  ; increase screen colour 
+;                inc BORDER  ; increase border colour
+;                jsr delay
+;                jmp start_cycle   ; repeat
+;
+;
+;delay           ldy #$0
+;rollover        inx
+;                bne rollover
+;                iny
+;                tya 
+;                jsr PRINTSR
+;                bne rollover
+;                rts
