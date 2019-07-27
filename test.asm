d2f7a844e07b2fa269ed12738e4a6c7589a98c27
Eric Whitestone <eric.whitestone@gmail.com>
2019-07-26 23:58:49 -0400

    minor fixes

--- test.asm
+++ test.asm
@@ -8,25 +8,25 @@ SCREEN = $d021
 PRINTSR = $ffd2
 SPRITE0_PTR = $07F8
 
+                ldx #$C9     ;set x to 0
+                stx SCREEN ;start screen color at 0
+                stx BORDER ;start border color at 0
+                jsr write_pattern ;write the sprite pattern in the 64 sprite bytes
+                ldx #$FA ;store FA in x to written to sprite pointer
+                stx SPRITE0_PTR ; write FA to be the value of the sprite pointer,
+                                ; this is the value that is used to derive $200 used as the start of the sprite data
+                ldx $FFFF
+loop            stx $D015   ; write FFFF to sprite enable 
+                jmp loop
 
-write_pattern   lda #$07        ;store the byte pattern for the sprite
+write_pattern   lda #$C7        ;store the byte pattern for the sprite
                 ldy #$0
-write_byte      sta $4640, Y      ; $8(segment multplier)*$64 + $4000
+write_byte      sta $3E80, Y      ; $10(segment multplier)*$40 
                 iny             ; increment the idx
-                cpy #$64        ; check if 64 bytes have been written
+                cpy #$40        ; check if 64 bytes have been written
                 bne write_byte  ; keep going until we hit 64 bytes
                 rts
 
-        ldx #$0     ;set x to 0
-        stx SCREEN ;start screen color at 0
-        stx BORDER ;start border color at 0
-        jsr write_pattern ;write the sprite pattern in the 64 sprite bytes
-        ldx #$25 ;store 8 in x,
-        stx SPRITE0_PTR ; write 8 to be the value of the sprite pointer,
-                        ; this is the value that is used to derive $200 used as the start of the sprite data
-        ldx $FFFF
-loop    stx $D015   ; write FFFF to sprite enable 
-        jmp loop
 
 ;start_cycle     inc SCREEN  ; increase screen colour 
 ;                inc BORDER  ; increase border colour
